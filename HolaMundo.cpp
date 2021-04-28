1bed09f1966e009d2c7443983079f3dea0948b1c
alejandromaya <79921944+alejandromaya@users.noreply.github.com>
2021-04-27 21:59:55 -0500

    Create HolaMundo.cpp

---
+++ HolaMundo.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "!Hola, mundo¡" << endl; // Imprimimos solo texto
+
+      char a[10] = "Hola"; //Creamos un arreglo de tamaño 10 y asignamos 4 letras dentro del arreglo
+      char b[10] = "Mundo"; //Creamos un arreglo de tamaño 10 y asignamos 5 letras dentro del arreglo
+
+    cout << "!"<< a <<", "<< b << "¡"<< endl; // Imprimimos los dos arreglos
+
+}
